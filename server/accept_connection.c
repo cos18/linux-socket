2285ead7e23d16fca3449f28253214170c9aadac
Sungwoo Park <codest99@gmail.com>
2020-06-08 01:51:00 +0900

    fix data type

--- server/accept_connection.c
+++ server/accept_connection.c
@@ -5,7 +5,7 @@ int32_t accept_connection(int32_t server_sock)
 {
     int32_t sock;
     struct sockaddr_in clientaddr;
-    int clilen = sizeof(clientaddr);
+    unsigned int clilen = sizeof(clientaddr);
 
     sock = accept(server_sock, (struct sockaddr *)&clientaddr, &clilen);
     if (sock < 0)
